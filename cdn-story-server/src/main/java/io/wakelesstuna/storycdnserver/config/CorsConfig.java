66c538df2be59cb39a2ec2092701f6aebba092ee
Oscar Forss <oscar.forss.1@gmail.com>
2022-01-20 10:12:09 +0100

    Add cors config

--- cdn-story-server/src/main/java/io/wakelesstuna/storycdnserver/config/CorsConfig.java
+++ cdn-story-server/src/main/java/io/wakelesstuna/storycdnserver/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package io.wakelesstuna.imagecdnserver.config;
+package io.wakelesstuna.storycdnserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
