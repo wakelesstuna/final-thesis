358c8aa3bc2b2b9479b65d44d1301d68829c4730
Oscar Forss <oscar.forss.1@gmail.com>
2022-01-24 11:14:25 +0100

    Add image pojo from userservicee

---
+++ cdn-image-server/src/main/java/io/wakelesstuna/imagecdnserver/resoruce/dto/ImageFile.java
@@ -0,0 +1,25 @@
+package io.wakelesstuna.imagecdnserver.resoruce.dto;
+
+import lombok.*;
+
+import java.util.UUID;
+
+/**
+ * Simple pojo for transfer file data to the external image server
+ *
+ * @author oscar.steen.forss
+ */
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ImageFile {
+    UUID userId;
+    String url;
+    String fileName;
+    String fileType;
+    Long fileSize;
+    byte[] data;
+}
